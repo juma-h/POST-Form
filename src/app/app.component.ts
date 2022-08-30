6b74476d7b32754d9a58cebe3d383fef1d147775
Juma-h <mitchfibi@gmail.com>
2022-08-30 15:24:51 +0300

    post form angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'post-form';
+  invoiceForm :FormGroup;
+
+  ngOnInit() {
+    this.invoiceForm = new FormGroup({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      message: new FormControl('')
+    });
+  }
+
+}
+
+
+
+
+interface Item {
+  item: string;
+  quantity: number;
+  price:number;
+  amount:number;
+}
+
+interface Address {
+  name: string;
+  address:number;
+  phone: number;
+  email: string;
+}
+interface Invoice{
+  invoiceNumber:string;
+  invoiceDate:string;
+  paymentDue:string;
 }
